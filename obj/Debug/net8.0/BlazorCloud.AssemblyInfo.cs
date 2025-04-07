528ace888c904fd1e0ce66e9d8562e554e0346df
Enrique Davila Zuazo <edavilaz@gmail.com>
2025-04-07 12:52:02 -0500

    Cambio pagina prueba

--- obj/Debug/net8.0/BlazorCloud.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorCloud.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorCloud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a52002c39cc8009b8dbe2386b17b4926fec20c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorCloud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorCloud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
